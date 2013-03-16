ca80c7b52dc30e37d7685978d34bf3bf078f75c6
Silvan Spross <silvan.spross@gmail.com>
2013-03-16 15:00:55 +0100

    added sorting and filtering to testproject

--- testproject/myapp/views.py
+++ testproject/myapp/views.py
@@ -8,9 +8,28 @@ class BookTable(TastyBackbonePieTableHelper):
     root_url = '/api/v1/book/'
     fields = [
         {
+            'key': 'id',
+            'label': '#',
+        },
+        {
             'key': 'name',
             'label': 'Name',
+            'order_by': 'name',
         },
+        {
+            'key': 'added',
+            'label': 'Added at',
+            'order_by': 'added',
+        },
+        {
+            'key': 'read',
+            'label': 'Read',
+            'template': '<% if (entry.get(\'read\') == true) { %>x<% } %>',
+            'order_by': 'read',
+        },
+    ]
+    additional_html_fields = [
+        '<a class="btn btn-mini" href="#" data-id="<%= entry.get(\'id\') %>"><i class="icon-trash"></i></a>',
     ]
 
 
